d4d47efc86f34957440cfa4fd020cd0f3cd603ff
IgorSusmelj <igor_7779@hotmail.com>
2012-03-10 18:52:57 +0100

    Added analyze_performance function: -outputs render time and fps

--- time.js
+++ time.js
@@ -6,3 +6,10 @@
 var $time = Date.now || function() {
   return +new Date;
 };
+function analyze_performance(){
+	postTime = $time();
+	performanceText = postTime-renderTime+" ms <br>";
+	performanceText +=((((19*fps)+(1000/(postTime-lastTime)))/20) | 0)+" fps";
+	performanceConsole.innerHTML=performanceText;
+	lastTime = $time();
+}
