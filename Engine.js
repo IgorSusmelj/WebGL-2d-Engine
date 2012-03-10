77267678436bb4da9b7678f70bf3f6bd71f80a37
IgorSusmelj <igor_7779@hotmail.com>
2012-03-10 18:53:33 +0100

    Added new variables for performance anlyzer and update render()

--- Engine.js
+++ Engine.js
@@ -7,19 +7,34 @@
  */
 
 var gl; //
-var canvas
+var canvas;
+var renderTime;
+var debugConsole, performanceConsole;
+var postTime;
+var lastTime;
+var fps;
+var performanceText;
+
 
 
 function initWebGL(){
+	performanceConsole = document.getElementById("performance_analyzer");
 	canvas = document.getElementById("GL-Canvas");
 	gl = WebGLUtils.setupWebGL(canvas);
+	
+	lastTime = $time;
+	fps = 60;
+	
 	render();
+	
 }
 
 function render(){
+	renderTime = $time();
 	window.requestAnimFrame(render, canvas);
 	gl.clearColor(0.0,0.0,0.0,1.0);
 	gl.enable(gl.DEPTH_TEST);
 	gl.depthFunc(gl.LEQUAL);
 	gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+	analyze_performance();
 }
