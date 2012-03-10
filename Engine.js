f5c5025e2f2e37c621007f51addafd2dfff4fb78
IgorSusmelj <igor_7779@hotmail.com>
2012-03-10 23:45:03 +0100

    Added Shader and draw Sprite for testing

--- Engine.js
+++ Engine.js
@@ -14,6 +14,14 @@ var postTime;
 var lastTime;
 var fps;
 var performanceText;
+var Vwidth;
+var Vheight;
+var programmObject;
+var vertexAttribLoc;
+var sprite;
+var vVertices;
+var vertexPosBufferObject;
+
 
 
 
@@ -23,7 +31,13 @@ function initWebGL(){
 	gl = WebGLUtils.setupWebGL(canvas);
 	gl.viewportWidth = canvas.width;
 	gl.viewportHeight= canvas.height;
+	gl.clearColor(0.0,0.0,0.0,1.0);
+	gl.enable(gl.DEPTH_TEST);
+	gl.depthFunc(gl.LEQUAL);
+	sprite = new Sprite();
 	
+	programmObject = simpleSetup(gl, "vertexShader", "fragmentShader", "vPosition", null, null);
+	//vertexAttribLoc = gl.getAttribLocation(programmObject, "vPosition");
 	//initializes variables for performace_analyzer
 	lastTime = $time;
 	fps = 60;
@@ -35,9 +49,21 @@ function initWebGL(){
 function render(){
 	renderTime = $time();
 	window.requestAnimFrame(render, canvas);//call drawback function for smooth animation. (60fps limit << screen refresh rate)
-	gl.clearColor(0.0,0.0,0.0,1.0);
-	gl.enable(gl.DEPTH_TEST);
-	gl.depthFunc(gl.LEQUAL);
+	
+	gl.viewport(0,0,gl.viewportWidth,gl.viewportHeight);
 	gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+	vertexAttribLoc = gl.getAttribLocation(programmObject, "vPosition");
+	
+	draw(sprite);
+
+	
 	analyze_performance();//function for performance information
 }
+
+
+
+
+
+
+
+
