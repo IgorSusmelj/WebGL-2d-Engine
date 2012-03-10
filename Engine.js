999775e09cc75491b859f6d276047810de8339d1
IgorSusmelj <igor_7779@hotmail.com>
2012-03-10 14:48:08 +0100

    Added a initWebGL function to initialize the WebGL conext Added a render function

--- Engine.js
+++ Engine.js
@@ -1,3 +1,25 @@
 /**
- * This will be the Engine
+ * Engine Made By Igor Susmelj
  */
+
+/**
+ * Variable declarations
+ */
+
+var gl; //
+var canvas
+
+
+function initWebGL(){
+	canvas = document.getElementById("GL-Canvas");
+	gl = WebGLUtils.setupWebGL(canvas);
+	render();
+}
+
+function render(){
+	window.requestAnimFrame(render, canvas);
+	gl.clearColor(0.0,0.0,0.0,1.0);
+	gl.enable(gl.DEPTH_TEST);
+	gl.depthFunc(gl.LEQUAL);
+	gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+}
