8b85d5baec7e9cc1b9ffc24ebd2ff3a189cae866
IgorSusmelj <igor_7779@hotmail.com>
2012-03-10 23:42:57 +0100

    Added function for drawing the Sprite

--- Sprite.js
+++ Sprite.js
@@ -8,19 +8,21 @@ function Sprite(){
 	this.itemSize = 3;
 	this.numItems = 4;
 	
-	gl.bindBuffer(gl.ARRAY_BUFFER,quad_buffer);
-	vertices = [
-	            1.0,   1.0,	0.0,
-	            -1.0,  1.0,	0.0,
-	            1.0,  -1.0,	0.0,
-	            -1.0, -1.0, 0.0
-	            
-	            ];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices),gl.STATIC_DRAW);
-	
+	gl.bindBuffer(gl.ARRAY_BUFFER,this.quad_buffer);
+	this.vertices = [
+	                 0.1,   0.1,	0.0,
+	                 -0.1,  0.1,	0.0,
+	                 0.1,  -0.1,	0.0,
+	                 -0.1, -0.1, 0.0
+	                 ];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices),gl.STATIC_DRAW);
+	return this.quad_buffer;
 }
 
-Sprite.prototype.get = function(){
-	return false;
-};
-
+function draw(_sprite){
+	gl.bindBuffer(gl.ARRAY_BUFFER,_sprite);
+	
+	gl.vertexAttribPointer(vertexAttribLoc,3,gl.FLOAT,false,0,0);
+	gl.enableVertexAttribArray(vertexAttribLoc);
+	gl.drawArrays(gl.TRIANGLE_STRIP,0,4);
+}
