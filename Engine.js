6c517e4ff8aec644c722a8adf1ef962fd0d2c8ea
IgorSusmelj <igor_7779@hotmail.com>
2012-03-10 19:00:49 +0100

    Added viewport parameters to the WebGL context

--- Engine.js
+++ Engine.js
@@ -21,20 +21,23 @@ function initWebGL(){
 	performanceConsole = document.getElementById("performance_analyzer");
 	canvas = document.getElementById("GL-Canvas");
 	gl = WebGLUtils.setupWebGL(canvas);
+	gl.viewportWidth = canvas.width;
+	gl.viewportHeight= canvas.height;
 	
+	//initializes variables for performace_analyzer
 	lastTime = $time;
 	fps = 60;
 	
-	render();
+	render();//main function for drawing
 	
 }
 
 function render(){
 	renderTime = $time();
-	window.requestAnimFrame(render, canvas);
+	window.requestAnimFrame(render, canvas);//call drawback function for smooth animation. (60fps limit << screen refresh rate)
 	gl.clearColor(0.0,0.0,0.0,1.0);
 	gl.enable(gl.DEPTH_TEST);
 	gl.depthFunc(gl.LEQUAL);
 	gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-	analyze_performance();
+	analyze_performance();//function for performance information
 }
