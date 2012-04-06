c3854bb359dfefc9991622431e5818bc2fd029df
Igor Susmelj <igor_7779@hotmail.com>
2012-04-06 17:47:10 +0200

    Updated Example to use 2 different textures

--- Example.js
+++ Example.js
@@ -2,13 +2,19 @@
  * 
  */
 
+var sprite;
+var example;
+
 function GAME_START(){
 	sprite = new Sprite("nehe.gif",256,256);
+	example= new Sprite("example_texture.jpg",64,64);
 	
 }
 
 function GAME_RENDER(){
-	DrawSprite(sprite,0.9,0.0);
+	DrawSprite(sprite,0.0,0.0);
+	DrawSprite(example,0.5,0.0);
+	//DrawText("test");
 }
 
 function GAME_END(){
