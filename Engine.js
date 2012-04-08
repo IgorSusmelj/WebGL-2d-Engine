801b277b49b656dd61dc880eb0ad0113248ec122
Igor Susmelj <igor_7779@hotmail.com>
2012-04-08 13:17:38 +0200

    Added flag for resolution independent rendering. -This option should give the Engine a better platform compatibility  with smartphones or small resolution devices.

--- Engine.js
+++ Engine.js
@@ -27,9 +27,19 @@ var pMatrix = mat4.create();
 var renderTimer = new Timer();
 var texture_test;
 
+var Mouse;
+var DEBUG_ENABLED;
+var RESOLUTION_INDEPENDENT_RENDERING;
 
 
-function initWebGL(){
+var pixels;
+
+
+function InitEngine(_ResolutionIndependent,_debug){
+	
+	DEBUG_ENABLED = _debug;
+	RESOLUTION_INDEPENDENT_RENDERING = _ResolutionIndependent;
+	
 	performanceConsole = document.getElementById("performance_analyzer");
 	canvas = document.getElementById("GL-Canvas");
 	gl = WebGLUtils.setupWebGL(canvas);
@@ -46,7 +56,13 @@ function initWebGL(){
 	lastTime = $time;
 	fps = 60;
 
+	//Init Mouse and MouseEvent listeners
+	Mouse = new EngineInitMouse();
 	
+	
+	canvas.onmousedown = EngineMouseDown;
+	document.onmouseup = EngineMouseUp;
+	document.onmousemove=EngineMouseMove;
 	
 	
 	initShaders();
@@ -54,31 +70,107 @@ function initWebGL(){
 	GAME_START();
 	
 	//mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
-	mat4.ortho(0, 1, 0, 1, 0.0, 100.0, pMatrix);
-	
+	UpdateViewport();
 	render();//main function for drawing
 	shutdown();
 }
 
 
+function UpdateViewport(){
+	mat4.ortho(0, 1, 0, 1, 0.0, 100.0, pMatrix);
+	gl.viewport(0,0,gl.viewportWidth,gl.viewportHeight);
+	
+	mat4.identity(mvMatrix);
+	//mat4.translate(mvMatrix,[0.0,0.0,-5.0]);
+	//mat4.transpose(mvMatrix, pMatrix);
+	
+
+}
+
+var EngineInitMouse = function(){
+	this.Sx = 0;
+	this.Sy = 0;
+	this.x = 0;
+	this.y = 0;
+	this.down = false;
+	this.move = false;
+	this.button = 0;
+};
+
+function EngineMouseDown(event){
+	Mouse.down = true;
+	Mouse.Sx = event.clientX;
+	Mouse.Sy = event.clientX;
+	Mouse.x = event.clientX;
+	Mouse.y = event.clientX;
+	switch(event.button){
+	case 0:
+		Mouse.button = 1;
+		break;
+	case 1:
+		Mouse.button = 3;
+		break;
+	case 2:
+		Mouse.button = 2;
+		break;
+	}
+}
+
+function EngineMouseUp(event){
+	Mouse.down = false;
+	Mouse.move = false;
+}
+
+function EngineMouseMove(event){
+	if(Mouse.down){
+		Mouse.move = true;
+		Mouse.x = event.clientX;
+		Mouse.y = event.clientX;
+	}
+}
+
+function MouseX(){
+	return Mouse.x;
+}
+function MouseY(){
+	return Mouse.y;
+}
+function MouseDown(key){
+	if(Mouse.down){
+		if(key==Mouse.button)
+			return  true;
+		return false;		
+	}
+
+}
 
 
 function render(){
-	postTime = $time();
-	renderTimer.start();
+	if(DEBUG_ENABLED)
+		{
+			postTime = $time();
+			renderTimer.start();		
+		}
 	window.requestAnimFrame(render, canvas);//call drawback function for smooth animation. (60fps limit << screen refresh rate)
 	
-	gl.viewport(0,0,gl.viewportWidth,gl.viewportHeight);
+	
 	gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
 	
 	
 	mat4.identity(mvMatrix);
-	mat4.translate(mvMatrix,[0.0,0.0,-5.0]);
+	//mat4.rotate(mvMatrix,Math.PI/32,[0,0,1]);
 	
 	GAME_RENDER();
+	
 
+	
+	
 
-	analyze_performance();//function for performance information
+	if(DEBUG_ENABLED)
+	{
+		analyze_performance();//function for performance information	
+	}
+
 }
 
 function shutdown(){
