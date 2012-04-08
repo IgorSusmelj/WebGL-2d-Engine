70b39447af85835e214f9bfba63bb49c07938c2d
Igor Susmelj <igor_7779@hotmail.com>
2012-04-08 13:13:55 +0200

    Added Mouse information for logger

--- Time.js
+++ Time.js
@@ -13,12 +13,25 @@ function analyze_performance(){
 	//postTime = $time();
 	var difference = postTime-lastTime;
 	
+	var tmpText;
+	
 	if(renderTime==0){
 		performanceText = "0000"+" microSeconds <br>";
 	}else{
 		performanceText = renderTime+" microSeconds<br>";
 	}
-	performanceText +=((((19*fps)+(1000/difference))/20) | 0)+" fps";
-	performanceConsole.innerHTML=performanceText;
+	performanceText +=((((19*fps)+(1000/difference))/20) | 0)+" fps<br>";
+	tmpText=performanceText+"<br>";
+	tmpText+="MouseStartX: "+Mouse.Sx+"<br>";
+	tmpText+="MouseStartY: "+Mouse.Sy+"<br>";
+	tmpText+="MouseX: "+MouseX()+"<br>";
+	tmpText+="MouseY: "+MouseY()+"<br>";
+	tmpText+="MouseDown: "+Mouse.down+"<br>";
+	tmpText+="MouseMove: "+Mouse.move+"<br>";
+	tmpText+="pixels: "+pixels+"<br>";
+	
+	pixels
+	
+	performanceConsole.innerHTML = tmpText;
 	lastTime = $time();
 }
