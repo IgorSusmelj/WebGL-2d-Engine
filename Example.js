fa49336d2ca3ce78f8e8009bd772201b3a3d3fac
IgorSusmelj <igor_7779@hotmail.com>
2012-04-14 13:59:33 +0200

    Updated example with a font demonstration

--- Example.js
+++ Example.js
@@ -4,9 +4,10 @@
 
 var sprite;
 var example;
+var font;
 
 function DOM_LOADED(){
-	InitEngine(false,false,true,false);
+	InitEngine(false,false,true,true);
 	
 }
 
@@ -14,13 +15,15 @@ function GAME_START(){
 	
 	sprite = LoadImage("nehe.gif",256,256);
 	example= LoadImage("example_texture.jpg",64,64);
-	
+	font = LoadFont("BitmapFont_Calibri.png");
 }
 
 function GAME_RENDER(){
 	DrawImage(sprite,0.0,0.0);
 	if(MouseDown(1))
 		DrawImage(example,0.5,0.0);
+	SetFont(font);
+	Text(0.6,0.6,"0512");
 	//DrawText("test");
 	
 }
