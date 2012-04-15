d10c33164c9e481570df47ebfa274f1dc27b2610
IgorSusmelj <igor_7779@hotmail.com>
2012-04-15 15:26:27 +0200

    Changed GAME_START to GAME_INIT

--- Example.js
+++ Example.js
@@ -6,22 +6,28 @@ var sprite;
 var example;
 var font;
 
+var audioTest;
+
 function DOM_LOADED(){
 	InitEngine(false,false,true,true);
 	
 }
 
-function GAME_START(){
+function GAME_INIT(){
 	
 	sprite = LoadImage("nehe.gif",256,256);
 	example= LoadImage("example_texture.jpg",64,64);
 	font = LoadFont("BitmapFont_Calibri.png");
+	audioTest = LoadSound("GetReadyForThis.mp3");
+	
 }
 
 function GAME_RENDER(){
 	DrawImage(sprite,0.0,0.0);
 	if(MouseDown(1))
-		DrawImage(example,0.5,0.0);
+		PlaySound(audioTest);
+	if(MouseDown(2))
+		FreeSound(audioTest);
 	SetFont(font);
 	Text(0.6,0.6,"0512");
 	//DrawText("test");
