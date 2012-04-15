2ac830cd6efad068f80339e3d2260209756d38ab
IgorSusmelj <igor_7779@hotmail.com>
2012-04-15 12:03:08 +0200

    Added a basic structure to the Sound lib

---
+++ Sound.js
@@ -0,0 +1,13 @@
+/*
+ * Sound API for audio playback
+ * for every audio file there will be a new Audio instance created for
+ * preloading the audio sources
+ */
+
+function LoadSound(src){
+	return new Audio(src);
+}
+
+function PlaySound(sound){
+	sound.play();
+}
