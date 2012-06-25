c2f7e7d42e29b9c090b0943baa52ab883b4f419d
IgorSusmelj <igor_7779@hotmail.com>
2012-06-25 15:59:04 +0200

    Fixed Example

--- Example.js
+++ Example.js
@@ -13,16 +13,16 @@ var rotStep=0.0;
 
 function DOM_LOADED(){
 	InitEngine(640,480,true,true);
-	
+
 }
 
 function GAME_INIT(){
-	
+
 	sprite = LoadImage("Resources/nehe.gif",256,256);
 	example= LoadImage("Resources/example_texture.jpg",64,64);
 	font = LoadFont("Resources/BitmapFont_Calibri.png");
 	audioTest = LoadSound("Resources/GetReadyForThis.mp3");
-	
+
 }
 
 function GAME_RENDER(){
@@ -32,14 +32,13 @@ function GAME_RENDER(){
 		rotStep+=0.05;
 	if(MouseDown(2))
 		rotStep-=0.05;
-	
+
 	if(KeyDown(38))
 		Text(0.6,0.6,"0512");
 	SetFont(font);
-	
+
 }
 
 function GAME_END(){
-	
-}
 
+}
